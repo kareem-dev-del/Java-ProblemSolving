7eedd38a5f0edbee09881a10cb5f8d8f70b20bd1
kareem-dev-del <krym33632@gmail.com>
2025-11-12 17:23:11 +0200

    New problem solving using java

--- src/FactorialRecursion/FactorialRecursion.java
+++ src/FactorialRecursion/FactorialRecursion.java
@@ -1,9 +1,24 @@
+/*
+ * البرنامج: حساب العامل (Factorial) باستخدام الاستدعاء الذاتي (Recursion)
+ * الوصف: يقوم هذا البرنامج بحساب العامل (Factorial) لعدد يُدخله المستخدم
+ *       عن طريق دالة تستدعي نفسها Recursively.
+ * اللغة: Java
+ * الكاتب: كريم
+ */
+
 package FactorialRecursion;
+import java.util.Scanner;
 
 public class FactorialRecursion {
     public static void main(String[] args) {
-        System.out.println("Kareem");
+     Scanner input = new Scanner(System.in);
+     System.out.println("Enter number ");
+     int num =input.nextInt();
+       System.out.println("Factoria: "+ num + " is " +Factorial(num));
 
-
+    }
+    static  int Factorial( int number){
+        if (number < 1) return 1;
+        else return number * Factorial(number - 1);
     }
 }
