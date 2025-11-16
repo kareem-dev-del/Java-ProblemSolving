c51ed025f74a2ebfd862b5d3a49219daf79d30c0
kareem-dev-del <krym33632@gmail.com>
2025-11-16 22:40:53 +0200

    Update

---
+++ src/Name.java
@@ -0,0 +1,77 @@
+//import java.util.Scanner;
+//
+//public class Name{
+//
+//    // Method 1: sum of three integers
+//    public static int sumThree(int a, int b, int c) {
+//        return a + b + c;
+//    }
+//
+//    // Method 2: sum of three floats
+//    public static float sumThree(float a, float b, float c) {
+//        return a + b + c;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Enter first number: ");
+//        float n1 = input.nextFloat();
+//
+//        System.out.print("Enter second number: ");
+//        float n2 = input.nextFloat();
+//
+//        System.out.print("Enter third number: ");
+//        float n3 = input.nextFloat();
+//
+//        // Call float version (since user may enter int/float)
+//        float result = sumThree(n1, n2, n3);
+//
+//        System.out.println("Sum = " + result);
+//    }
+//}
+// تعريف enum باسم Day
+//enum Day {
+//    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+//}
+//
+//public class Name {
+//    public static void main(String[] args) {
+//
+//        // اختيار يوم من enum
+//        Day today = Day.WEDNESDAY;
+//
+//        // استخدام switch لعرض رسالة حسب اليوم
+//        switch (today) {
+//            case MONDAY:
+//                System.out.println("Start of the work week!");
+//                break;
+//            case TUESDAY:
+//                System.out.println("Keep going, it's Tuesday!");
+//                break;
+//            case WEDNESDAY:
+//                System.out.println("Midweek day!");
+//                break;
+//            case THURSDAY:
+//                System.out.println("Almost Friday!");
+//                break;
+//            case FRIDAY:
+//                System.out.println("Last workday of the week!");
+//                break;
+//            case SATURDAY:
+//                System.out.println("Weekend time!");
+//                break;
+//            case SUNDAY:
+//                System.out.println("Rest and recharge!");
+//                break;
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
